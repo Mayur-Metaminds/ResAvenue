74fa37add2eb98c6f6c2ee65c7ca18389261a4ae
IndranilsDas <154364743+IndranilsDas@users.noreply.github.com>
2026-07-02 14:18:44 +0530

    new lottie event management homepage bento

--- apps/ui/src/lib/lottie-urls.ts
+++ apps/ui/src/lib/lottie-urls.ts
@@ -29,7 +29,7 @@ export const DIRECT_CONNECT_INNER_URL = `${LANDING}/direct_connect_inner.json`
 export const CHANNEL_CONNECT_INNER_URL = `${LANDING}/channel_connect_inner.json`
 export const PROPERTY_MANAGEMENT_INNER_URL = `${LANDING}/property_management_inner.json`
 export const DISTRIBUTION_NETWORK_INNER_URL = `${LANDING}/distribution_network_inner.json`
-export const EVENT_INNER_URL = `${LANDING}/event_and_ticketing_inner.json`
+export const EVENT_INNER_URL = `${LANDING}/event_management_new.json`
 export const HOTEL_WEBSITE_INNER_URL = `${LANDING}/hotel_website_builder_inner.json`
 export const REVENUE_MANAGEMENT_INNER = `${LANDING}/revenue_management_inner.json`
 
