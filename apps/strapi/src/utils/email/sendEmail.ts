de86e1377603c25fec40a8510e7db6b45e8bff2f
Yuvraj9769 <yuvrajs22it@student.mes.ac.in>
2026-09-04 18:31:10 +0530

    fix: removed test mail

--- apps/strapi/src/utils/email/sendEmail.ts
+++ apps/strapi/src/utils/email/sendEmail.ts
@@ -1,35 +1,31 @@
-
 import { env } from "./env"
 import { emailTransporter } from "./transporter"
 
 interface SendEmailOptions {
-    to?: string | string[];
-    subject: string;
-    text?: string;
-    html?: string;
-    attachments?: {
-        filename: string
-        path: string
-        contentType?: string
-    }[]
+  to?: string | string[]
+  subject: string
+  text?: string
+  html?: string
+  attachments?: {
+    filename: string
+    path: string
+    contentType?: string
+  }[]
 }
 
 export const sendEMail = async ({
-    // to = env.smtp.to,
-    to = "yuvrajs22it@student.mes.ac.in",
+  to = env.smtp.to,
+  subject,
+  text,
+  html,
+  attachments,
+}: SendEmailOptions) => {
+  await emailTransporter.sendMail({
+    from: env.smtp.user,
+    to,
     subject,
     text,
     html,
     attachments,
-  }: SendEmailOptions) => {
-
-    await emailTransporter.sendMail({
-        from: env.smtp.user,
-        to,
-        subject,
-        text,
-        html,
-        attachments,
-    })
-
-  }
+  })
+}
