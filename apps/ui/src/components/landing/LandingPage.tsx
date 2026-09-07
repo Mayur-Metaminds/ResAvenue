b5608e4b623997a8bf362be81e84d2738aa71340
Yuvraj9769 <yuvrajs22it@student.mes.ac.in>
2026-09-07 19:37:14 +0530

    chore: testing hostinger vps deploy v2

--- apps/ui/src/components/landing/LandingPage.tsx
+++ apps/ui/src/components/landing/LandingPage.tsx
@@ -16,7 +16,7 @@ import { CtaSection } from "../common/CtaSection"
 
 export default function LandingPage() {
   return (
-    <div className="bg-green-600">
+    <div className="bg-blue-600">
       <HeroSection />
       <Marquee<TrustedLogo>
         className="w-full pt-[30px] pb-[30px]"
